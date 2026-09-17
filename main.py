c63c586c084c1d6d5c802a347dbea511c42d1c16
Diana Dvoryak <dianadvoryak.w@gmail.com>
2026-09-17 21:17:44 +0200

    init

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from src.api import router as auth_router
 
 app = FastAPI(
-    title="online store catalog and search microservice",
-    description="микросевис каталога и поиска интернет магазина на FastAPI",
+    title="Microservice for catalog and search for an online electronics store",
+    description="микросервис каталога и поиска интернет магазина техники на FastAPI",
     version="1.0.0",
     debug=True
 )
